86acef3f5937b4beadfe411e14e76afc6fdda179
Yan Soesilo <yan_soesilo@hotmail.com>
2018-06-27 17:08:14 +0000

    initial commit

---
+++ src/components/CryptoCcyList.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import numeral from 'numeral';
+
+import mockData from '../api/data';
+import {getCryptoData} from '../api/api';
+
+const css = require('./CryptoCcyList.css');
+
+export default class CryptoCcyList extends React.Component {
+    constructor() {
+        super();
+        this.state = {};
+    }
+
+    render() {
+        const data = mockData;
+        return (
+            <div className={css.cryptoRow}>
+                <div className={css.cryptoName}>{data.name}</div>
+                <div className={css.cryptoPrice}>{`SGD ${numeral(data.price_sgd).format('0,0.00')}`}</div>
+                <div className={css.cryptoChange}>{`${data.percent_change_24h}%`}</div>
+            </div>
+        );
+    }
+}
